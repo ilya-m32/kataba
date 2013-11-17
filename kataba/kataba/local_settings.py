03a4a0a2f9e2f429d89983910c286d17f24286a7
Ktan32 <ilya@calculate.local>
2013-11-17 11:03:42 +0000

    Small dev. update

---
+++ kataba/kataba/local_settings.py
@@ -0,0 +1,29 @@
+# Example of local settings
+# Should be renamed to local_settings.py
+DJANGO_DIR='/home/ilya/Django/kataba' # path to dir with project
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'kataba_db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = 'Asia/Yekaterinburg'
+
+LANGUAGE_CODE = 'ru-RU'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'nm%=^)wn3w^7s(*ta)+oyu6i9w1=tv)bjwp1ttbk0z+a&amp;or06l'
