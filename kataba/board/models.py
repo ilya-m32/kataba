878377fc65806c9d2ac95e821e7c3cfbf45ef6ab
Ktan <aske.nazgul@gmail.com>
2013-12-26 19:27:33 +0600

    Fixed models, method delete() now also removes images and thumbnails

--- kataba/board/models.py
+++ kataba/board/models.py
@@ -2,6 +2,8 @@
 from django.db import models
 from django import forms
 from captcha.fields import CaptchaField
+from django.conf import settings
+import os
 
 class board(models.Model):
 	name = models.CharField(max_length=4)
@@ -17,21 +19,27 @@ class post_template(models.Model):
 	board_id = models.ForeignKey('board')
 	image = models.ImageField(upload_to='.')
 	
+	def delete(self,using=None):
+		""" Rewrited version which also deletes images and thumbnails """
+		if (self.image):
+			os.remove(settings.MEDIA_ROOT+'/'+self.image.name)
+			os.remove(settings.MEDIA_ROOT+'/thumbnails/'+self.image.name)
+		super(post_template,self).delete(using)
+	
 	def __unicode__(self):
 		return self.topic+': '+self.text[:40]+', '+str(self.date)
 	
 	
-	
 class thread(post_template):
 	def __init__(self,*args,**kwargs):
-		super(post_template,self).__init__(*args,**kwargs)
+		super(thread,self).__init__(*args,**kwargs)
 
 	post_count = models.IntegerField(default=0)
 	update_time = models.DateTimeField('%Y-%m-%d %H:%M:%S',auto_now=False)
 
 class post(post_template):
 	def __init__(self,*args,**kwargs):
-		super(post_template,self).__init__(*args,**kwargs)
+		super(post,self).__init__(*args,**kwargs)
 	
 	thread_id = models.ForeignKey('thread')	
 	sage = models.BooleanField(default=False)
